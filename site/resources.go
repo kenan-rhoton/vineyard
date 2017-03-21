91ec0114a97bec21aeab70b92e6a233691ddb36f
Kenan Rhoton <krhoton@redsauce.net>
2017-03-21 11:59:55 +0100

    format errors and remove executable

--- site/resources.go
+++ site/resources.go
@@ -2,17 +2,8 @@ package site
 
 import (
     "net/http"
-    "github.com/kenan-rhoton/vineyard/models"
 )
 
 func ResourcesSetup() {
-    http.Handle(
-        "/public/",
-        http.StripPrefix(
-            "/public/",
-            http.FileServer(
-                http.Dir("public")
-            )
-        )
-    )
+    http.Handle("/public/",http.StripPrefix("/public/",http.FileServer(http.Dir("public"))))
 }
